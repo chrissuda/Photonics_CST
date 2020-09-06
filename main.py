1f12b2278fa781fec7d95a2045beb8a1c69ab67b
chrissuda <ab792638826@gmail.com>
2020-09-05 22:03:40 -0400

    modify

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import torch
 from util import*
 from torchData import*
-from model import CSTModel
+from model import CSTModel2
 import wandb
 
-batch_size=256
+batch_size=128
 epochs=50
 
 train_labelFile="./data/label.json"
@@ -13,16 +13,18 @@ mean,std=getMeanStd(train_labelFile)
 
 print("mean:",mean," std:",std)
 
-trainset=CSTData(train_labelFile,mean=mean,std=std)
-valset=CSTData(val_labelFile,mean=mean,std=std)
+trainset=CSTData(train_labelFile)
+valset=CSTData(val_labelFile)
+
 
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
                                           shuffle=True)
-valloader = torch.utils.data.DataLoader(valset, batch_size=batch_size,
-                                         shuffle=False)
+
+valloader = torch.utils.data.DataLoader(valset, batch_size=5,
+                                         shuffle=True)
 
 #Set up the model
-model=CSTModel(2,10,50,50,4)
+model=CSTModel2(2,10,50,4)
 for param in model.parameters():
         param.requires_grad = True
         
@@ -33,9 +35,9 @@ if torch.cuda.is_available():
 else:
 	device = torch.device('cpu')
 
-optimizer=torch.optim.Adam(model.parameters())
+optimizer=torch.optim.Adam(model.parameters(),lr=0.0005)
 criterion = torch.nn.MSELoss()
-model=train(model,optimizer,trainloader,valloader,criterion,device,isWandb=True,epochs=epochs)
+model=train(model,optimizer,trainloader,valloader,criterion,device,isWandb=False,epochs=epochs)
 
 print("Finish")
 torch.save(model,"model.pt")
